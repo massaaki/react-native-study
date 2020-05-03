6de86653016f00771e999068d5cb863a007d11ad
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-05-03 16:01:16 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to react native</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    textAlign: 'center',
+    color: '#333',
+    marginBottom: 5,
+  },
+});
+
+export default App;
