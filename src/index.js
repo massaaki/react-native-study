19c031dac6eb8c0989f26adaaf31036fb13d436e
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio-7.local>
2020-05-05 19:48:43 -0300

    routes

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
+// import 'react-native-gesture-handler';
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {View} from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    textAlign: 'center',
-    color: '#333',
-    marginBottom: 5,
-  },
-});
-
-console.tron.log('hello world 2');
-
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to react native</Text>
-    </View>
-  );
+  return <View />;
 }
---
+++ src/pages/main/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+// import { Container } from './styles';
+
+const main = () => {
+  return <View />;
+};
+
+export default main;
